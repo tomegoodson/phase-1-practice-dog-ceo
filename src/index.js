47023501c3b7b8d3655a4ead581781f11b8f5d2f
tomegoodson <158308016+tomegoodson@users.noreply.github.com>
2024-03-19 14:03:45 -0500

    dog.md

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
 console.log('%c HI', 'color: firebrick')
+
+const breedUrl = "https://dog.ceo/api/breeds/list/all";
+
+fetch(breedUrl)
+  .then(response => response.json())
+  .then(data => {
+    const breeds = data.message;
+    const breedsList = document.getElementById('dog-breeds');
+
+    for (const breed in breeds) {
+      const li = document.createElement('li');
+      li.textContent = breed;
+      breedsList.appendChild(li);
+
+      li.addEventListener('click', () => {
+        li.style.color = 'firebrick';
+      });
+    }
+  });
+  document.getElementById('breed-dropdown').addEventListener('change', event => {
+    const letter = event.target.value;
+    const breedsList = document.getElementById('dog-breeds');
+    const allBreeds = breedsList.querySelectorAll('li');
+  
+    allBreeds.forEach(breed => {
+      breed.style.display = breed.textContent.startsWith(letter) ? '' : 'none';
+    });
+  });
